2fb12e03a5912c7cc7272346862d40e0fcdc72f4
Loptr <tjahn0612@gmail.com>
2025-07-30 14:00:23 +0200

    Implement Settings Generator

---
+++ maprando-plando/src/layout/settings_gen.rs
@@ -0,0 +1,30 @@
+use egui::Ui;
+use strum::VariantArray;
+
+pub trait SettingsGen {
+    fn generate<S: Into<String>>(&mut self, label: S, ui: &mut Ui);
+}
+
+impl SettingsGen for bool {
+    fn generate<S: Into<String>>(&mut self, label: S, ui: &mut Ui) {
+        ui.label(label.into());
+        ui.selectable_value(self, false, "No");
+        ui.selectable_value(self, true, "Yes");
+        ui.end_row();
+    }
+}
+
+pub trait SettingsPreset<K: VariantArray + PartialEq + Clone + ToString> where Self: Sized + PartialEq {
+    fn generate<S: Into<String>>(&mut self, label: S, ui: &mut Ui) {
+        ui.label(label.into());
+
+        for var in K::VARIANTS {
+            let preset = Self::get(var.clone());
+            if ui.selectable_label(*self == preset, var.to_string()).clicked() {
+                *self = preset;
+            }
+        }
+    }
+
+    fn get(key: K) -> Self;
+}
