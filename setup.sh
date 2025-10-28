bd621f3f4e364f9fdf3a8c472695fa7f655a83ed
Loptr <tjahn0612@gmail.com>
2025-10-28 06:12:41 +0100

    Update setup script for new project structure

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 echo Copying files from MapRandomizer...
-cd maprando-plando
 mkdir -p data/maprando-data
 mkdir -p data/maps
 
-cp -r ../MapRandomizer/rust/data ./data/maprando-data
-cp -r ../MapRandomizer/patches ./data
-cp -r ../MapRandomizer/gfx ./data
-cp -r ../MapRandomizer/visualizer ./data
-cp -r ../MapRandomizer/MapRandoSprites ./data
-cp -r ../MapRandomizer/sm-json-data ./data
-cp ../MapRandomizer/room_geometry.json ./data/room_geometry.json
+cp -r ./MapRandomizer/rust/data ./data/maprando-data
+cp -r ./MapRandomizer/patches ./data
+cp -r ./MapRandomizer/gfx ./data
+cp -r ./MapRandomizer/visualizer ./data
+cp -r ./MapRandomizer/MapRandoSprites ./data
+cp -r ./MapRandomizer/sm-json-data ./data
+cp ./MapRandomizer/room_geometry.json ./data/room_geometry.json
 mkdir -p ./data/TitleScreen/Images
-cp -r ../MapRandomizer/TitleScreen/Images ./data/TitleScreen
-cp -r ../MapRandomizer/maps/vanilla ./data/maps
+cp -r ./MapRandomizer/TitleScreen/Images ./data/TitleScreen
+cp -r ./MapRandomizer/maps/vanilla ./data/maps
