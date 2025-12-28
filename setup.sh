013743075503d0f424c2800fdee177cd56679de9
Loptr <tjahn0612@gmail.com>
2025-12-28 02:07:39 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ mkdir -p data/maps
 cp -r ./MapRandomizer/rust/data ./data/maprando-data
 cp -r ./MapRandomizer/patches ./data
 cp -r ./MapRandomizer/gfx ./data
-cp -r ./MapRandomizer/visualizer ./data
 cp -r ./MapRandomizer/MapRandoSprites ./data
 cp -r ./MapRandomizer/sm-json-data ./data
 cp ./MapRandomizer/room_geometry.json ./data/room_geometry.json
